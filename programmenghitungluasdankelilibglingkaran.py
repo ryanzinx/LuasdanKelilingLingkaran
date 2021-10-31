6142b21b71fa6a3e1b0fbdeb809205de3c573a3a
ryanzinx <92358422+ryanzinx@users.noreply.github.com>
2021-11-01 01:17:32 +0700

    Add files via upload

---
+++ programmenghitungluasdankelilibglingkaran.py
@@ -0,0 +1,9 @@
+print("Program Menghitung Luas dan Keliling Lingkaran")
+r = int(input("Masukan Jari - Jari = "))
+
+phi = 22/7
+luas = phi*r*r
+kel = 2*phi*r
+
+print("Luas Lingkaran adalah        = ", luas)
+print("Keliling Lingkaran adalah    = ", kel)
